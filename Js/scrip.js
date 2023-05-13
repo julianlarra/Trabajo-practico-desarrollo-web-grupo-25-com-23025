e4daacb9629bdca67339fec3373a3c7d0d8d9da7
julianlarra <larragueta.julian@gmail.com>
2023-05-12 22:20:45 -0300

    punto de corte menu 932 listo

--- Js/scrip.js
+++ Js/scrip.js
@@ -19,10 +19,10 @@ function carousel() {
 
 
 
-/*  
+
 function accion(){
     console.log('esta funcionando mi boton')
     var ancla = document.getElementsByClassName('nav-enlace');
     for (var i = 0; i < ancla.length; i++) { ancla[i].classList.toggle('desaparece')}
     
-    } */
+    } 
