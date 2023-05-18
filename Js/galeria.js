50d378ecb903f91b1e27ac57a60d7faabf3afc2a
julianlarra <larragueta.julian@gmail.com>
2023-05-18 10:01:37 -0300

    se completo la galeria de imagen e incorporo la hoja galeria con sus estilos y acciones

---
+++ Js/galeria.js
@@ -0,0 +1,320 @@
+const obra1 = document.getElementById('obra1');
+let isAmpliada = false;
+
+obra1.addEventListener('click', () => {
+  if (isAmpliada) {
+    obra1.classList.remove('ampliada');
+  } else {
+    obra1.classList.add('ampliada');
+    } 
+    
+isAmpliada = !isAmpliada;
+});
+
+
+const obra2 = document.getElementById('obra2');
+
+obra2.addEventListener('click', () => {
+  if (isAmpliada) {
+    obra2.classList.remove('ampliada');
+  } else {
+    obra2.classList.add('ampliada');
+    } 
+    
+isAmpliada = !isAmpliada;
+});
+
+
+const obra3 = document.getElementById('obra3');
+
+obra3.addEventListener('click', () => {
+  if (isAmpliada) {
+    obra3.classList.remove('ampliada');
+  } else {
+    obra3.classList.add('ampliada');
+    } 
+    
+isAmpliada = !isAmpliada;
+});
+
+
+const obra4 = document.getElementById('obra4');
+
+obra4.addEventListener('click', () => {
+  if (isAmpliada) {
+    obra4.classList.remove('ampliada');
+  } else {
+    obra4.classList.add('ampliada');
+    } 
+    
+isAmpliada = !isAmpliada;
+});
+
+
+const obra5 = document.getElementById('obra5');
+
+obra5.addEventListener('click', () => {
+  if (isAmpliada) {
+    obra5.classList.remove('ampliada');
+  } else {
+    obra5.classList.add('ampliada');
+    } 
+    
+isAmpliada = !isAmpliada;
+});
+
+
+const obra6 = document.getElementById('obra6');
+
+obra6.addEventListener('click', () => {
+  if (isAmpliada) {
+    obra6.classList.remove('ampliada');
+  } else {
+    obra6.classList.add('ampliada');
+    } 
+    
+isAmpliada = !isAmpliada;
+});
+
+
+const obra7 = document.getElementById('obra7');
+
+obra7.addEventListener('click', () => {
+  if (isAmpliada) {
+    obra7.classList.remove('ampliada');
+  } else {
+    obra7.classList.add('ampliada');
+    } 
+    
+isAmpliada = !isAmpliada;
+});
+
+
+const obra8 = document.getElementById('obra8');
+
+obra8.addEventListener('click', () => {
+  if (isAmpliada) {
+    obra8.classList.remove('ampliada');
+  } else {
+    obra8.classList.add('ampliada');
+    } 
+    
+isAmpliada = !isAmpliada;
+});
+
+
+const obra9 = document.getElementById('obra9');
+
+obra9.addEventListener('click', () => {
+  if (isAmpliada) {
+    obra9.classList.remove('ampliada');
+  } else {
+    obra9.classList.add('ampliada');
+    } 
+    
+isAmpliada = !isAmpliada;
+});
+
+
+const obra10 = document.getElementById('obra10');
+
+obra10.addEventListener('click', () => {
+  if (isAmpliada) {
+    obra10.classList.remove('ampliada');
+  } else {
+    obra10.classList.add('ampliada');
+    } 
+    
+isAmpliada = !isAmpliada;
+});
+
+
+const obra11 = document.getElementById('obra11');
+
+obra11.addEventListener('click', () => {
+  if (isAmpliada) {
+    obra11.classList.remove('ampliada');
+  } else {
+    obra11.classList.add('ampliada');
+    } 
+    
+isAmpliada = !isAmpliada;
+});
+
+
+const obra12 = document.getElementById('obra12');
+
+obra12.addEventListener('click', () => {
+  if (isAmpliada) {
+    obra12.classList.remove('ampliada');
+  } else {
+    obra12.classList.add('ampliada');
+    } 
+    
+isAmpliada = !isAmpliada;
+});
+
+
+const obra13 = document.getElementById('obra13');
+
+obra13.addEventListener('click', () => {
+  if (isAmpliada) {
+    obra13.classList.remove('ampliada');
+  } else {
+    obra13.classList.add('ampliada');
+    } 
+    
+isAmpliada = !isAmpliada;
+});
+
+
+const obra14 = document.getElementById('obra14');
+
+obra14.addEventListener('click', () => {
+  if (isAmpliada) {
+    obra14.classList.remove('ampliada');
+  } else {
+    obra14.classList.add('ampliada');
+    } 
+    
+isAmpliada = !isAmpliada;
+});
+
+
+const obra15 = document.getElementById('obra15');
+
+obra15.addEventListener('click', () => {
+  if (isAmpliada) {
+    obra15.classList.remove('ampliada');
+  } else {
+    obra15.classList.add('ampliada');
+    } 
+    
+isAmpliada = !isAmpliada;
+});
+
+
+const obra16 = document.getElementById('obra16');
+
+obra16.addEventListener('click', () => {
+  if (isAmpliada) {
+    obra16.classList.remove('ampliada');
+  } else {
+    obra16.classList.add('ampliada');
+    } 
+    
+isAmpliada = !isAmpliada;
+});
+
+
+const obra17 = document.getElementById('obra17');
+
+obra17.addEventListener('click', () => {
+  if (isAmpliada) {
+    obra17.classList.remove('ampliada');
+  } else {
+    obra17.classList.add('ampliada');
+    } 
+    
+isAmpliada = !isAmpliada;
+});
+
+
+const obra18 = document.getElementById('obra18');
+
+obra8.addEventListener('click', () => {
+  if (isAmpliada) {
+    obra18.classList.remove('ampliada');
+  } else {
+    obra18.classList.add('ampliada');
+    } 
+    
+isAmpliada = !isAmpliada;
+});
+
+
+const obra19 = document.getElementById('obra19');
+
+obra19.addEventListener('click', () => {
+  if (isAmpliada) {
+    obra19.classList.remove('ampliada');
+  } else {
+    obra19.classList.add('ampliada');
+    } 
+    
+isAmpliada = !isAmpliada;
+});
+
+
+const obra20 = document.getElementById('obra20');
+
+obra20.addEventListener('click', () => {
+  if (isAmpliada) {
+    obra20.classList.remove('ampliada');
+  } else {
+    obra20.classList.add('ampliada');
+    } 
+    
+isAmpliada = !isAmpliada;
+});
+
+
+const obra21 = document.getElementById('obra21');
+
+obra21.addEventListener('click', () => {
+  if (isAmpliada) {
+    obra21.classList.remove('ampliada');
+  } else {
+    obra21.classList.add('ampliada');
+    } 
+    
+isAmpliada = !isAmpliada;
+});
+
+
+const obra22 = document.getElementById('obra22');
+
+obra22.addEventListener('click', () => {
+  if (isAmpliada) {
+    obra22.classList.remove('ampliada');
+  } else {
+    obra22.classList.add('ampliada');
+    } 
+    
+isAmpliada = !isAmpliada;
+});
+
+
+const obra23 = document.getElementById('obra23');
+
+obra23.addEventListener('click', () => {
+  if (isAmpliada) {
+    obra23.classList.remove('ampliada');
+  } else {
+    obra23.classList.add('ampliada');
+    } 
+    
+isAmpliada = !isAmpliada;
+});
+
+
+const obra24 = document.getElementById('obra24');
+
+obra24.addEventListener('click', () => {
+  if (isAmpliada) {
+    obra24.classList.remove('ampliada');
+  } else {
+    obra24.classList.add('ampliada');
+    } 
+    
+isAmpliada = !isAmpliada;
+});
+
+
+
+function accion(){
+  console.log('esta funcionando mi boton')
+  var ancla = document.getElementsByClassName('nav-enlace');
+  for (var i = 0; i < ancla.length; i++) { ancla[i].classList.toggle('desaparece')}
+  
+  } 
